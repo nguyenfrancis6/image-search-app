19a6a881ab8cdb91f4ede117ea9cc01bf55a3846
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-01-05 22:23:05 -0800

    base look and styling, responsiveness

---
+++ script.js
@@ -0,0 +1,6 @@
+const accessKey = '7VRY1KxgB_2YoIPdo2kEisIvvkagdSKyMSRUx4VL-U8'
+
+const formEl = document.querySelector('form')
+const inputEl = document.getElementById('search-input')
+const searchResults = document.querySelector('.search-results')
+const showMore = document.getElementById('show-more-button')
